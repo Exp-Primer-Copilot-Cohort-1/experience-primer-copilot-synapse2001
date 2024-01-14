248d882f7e295ec8630306d7a491447db0eae436
Gagan Agarwal <73771646+synapse2001@users.noreply.github.com>
2024-01-14 08:12:21 +0000

    fix2

--- comments.js
+++ comments.js
@@ -1,11 +1,25 @@
-// Create Web Server
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+// Create web server
+// 1. create a web server
+// 2. read the file comments.json and then send the content back to the client
+// 3. if any error occurs, send an error message back to the client
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var ROOT_DIR = "html/";
+http.createServer(function (req, res) {
+	// parse URL
+	var urlObj = url.parse(req.url, true, false);
+	console.log('URL path: ' + urlObj.pathname);
+	console.log('URL search: ' + urlObj.search);
+	// Read the file
+	fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
+		if (err) {
+			res.writeHead(404);
+			res.end(JSON.stringify(err));
+			return;
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
+}).listen(8080);
+console.log('Server running at http://localhost:8080/');
