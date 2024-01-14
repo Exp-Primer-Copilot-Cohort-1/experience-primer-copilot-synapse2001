51cfb3c776374fad8e0d01f6fe71d25e7bf36115
Gagan Agarwal <73771646+synapse2001@users.noreply.github.com>
2024-01-14 08:08:20 +0000

    fixed

--- comments.js
+++ comments.js
@@ -1,9 +1,9 @@
+// Create Web Server
 const http = require('http');
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+    res.end('Hello, World!');
 });
 
 server.listen(3000, 'localhost', () => {
